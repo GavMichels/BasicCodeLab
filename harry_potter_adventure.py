c4d106d84779f89416aeaac5302bf96a3b28655f
GavMichels <164392567+GavMichels@users.noreply.github.com>
2024-04-03 13:45:38 -0400

    Add files via upload

---
+++ harry_potter_adventure.py
@@ -0,0 +1,26 @@
+print("Welcome to Hogwarts! Where would you like to go?")
+print("1. Forbidden Forest")
+print("2. Hagrid's Hut")
+print("3. Quidditch Pitch")
+location = input("Choose a number: ")
+
+if location == "1":
+    print("You've entered the Forbidden Forest. It's dark and eerie")
+elif location == "2":
+    print("You're at Hagrid's Hut. Hagrid greets you warmly.")
+elif location == "3":
+    print("Welcome to the Quidditch Pitch. Time for some Quidditch!")
+
+if location == "1":
+    print("Your boots have been caught in mud, You are forced to leave them behind, You have decided to leave")
+elif location == "2":
+    print("You knock on the door of Hagrid's Hut but you hear nothing, You then look through the window and see Hagrid playing Fortnite, You decide to leave considering you know he won't make time for you")
+elif location == "3":
+    print("You decide to play quidditch but you get hurt in the process and are know paralyzed from the waste down, you are unable to perform any task")
+
+if location == "1":
+    print("Game Over")
+elif location == "2":
+    print("Game Over")
+elif location == "3":
+    print("Game Over")
